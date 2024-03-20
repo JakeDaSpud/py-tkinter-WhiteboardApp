5907955d7cc2a1f3cde1e77b32ea46bd8643c1d0
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2024-03-20 20:07:27 +0000

    - Finished non-GUI functions
    - Added doc comments to all functions

--- whiteboard.py
+++ whiteboard.py
@@ -4,15 +4,33 @@
 import tkinter as tk
 from tkinter.colorchooser import askcolor
 
-# Create blank drawing / canvas
 def start_drawing(event):
-    # Global Variables
+    """
+    Set is_drawing to True
+    @param: event The mouse input data
+    """
+
     global is_drawing, prev_x, prev_y
 
     is_drawing = True
     prev_x, prev_y = event.x, event.y # Mouse cursor position recorded
 
+def stop_drawing(event):
+    """
+    Set is_drawing to False
+    @param: event The mouse input data
+    """
+
+    global is_drawing
+
+    is_drawing = False
+
 def draw(event):
+    """
+    Draw coloured vectors to canvas
+    @param: event The mouse input data
+    """
+
     global is_drawing, prev_x, prev_y
 
     if is_drawing:
@@ -23,4 +41,24 @@ def draw(event):
         
         prev_x, prev_y = event.x, event.y # Mouse cursor position recorded
 
-print("Exiting app.")
+def change_pen_color():
+    """
+    Use tkinter's askcolor() to change canvas pen colour
+    """
+
+    global drawing_color
+    
+    color = askcolor()[1] # askcolor() is from tkinter to change canvas pen colour
+    if color: # Not null
+        drawing_color = color
+
+def change_line_width(value):
+    """
+    Change tkinter's canvas pen thickness
+    @param: value New canvas pen size
+    """
+
+    global line_width
+    line_width = int(value)
+
+print("Exiting WhiteboardApp, goodbye!")
